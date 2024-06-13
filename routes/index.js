41a4549de51bd321ea19be4882df47272747841e
Mukul Jataria <mukul.jataria@indiamart.com>
2024-06-14 00:20:35 +0530

    Added routes

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,19 @@ const express = require('express');
 
 const router = express.Router();
 const homeController = require('../controllers/home_controller')
+const postController = require('../controllers/posts_controller')
 
 console.log('router loaded');
 
 router.get('/', homeController.home)
+router.get('/posts',postController.post)
+router.get('/index', homeController.index)
+router.use('/users',require('./users'))
+
+
+
+//for any further routes, access from here
+// router.use('/routerName', require(./routerfile));
 
 
  
