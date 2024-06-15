2bcea92764daec7dd6617bacfb0e53c5d9c60e8b
Mukul Jataria <mukul.jataria@indiamart.com>
2024-06-15 16:23:26 +0530

    Setup static files access for layout and pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,23 @@
 module.exports.profile = function(req,res){
-    res.end('<h1>User Profile</h1>');
+   return res.render('users_profile',{
+    title : "Profile"
+   })
 }
 
 module.exports.privacy = function(req,res){
     res.end('<h1>User Privacy</h1>');
 }
+
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title: "Codiel | Sign Up"
+    })
+}
+
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title: "Codiel | Sign In"
+    })
+}
+
+
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express'); 
 const router = express.Router();
 
-const userController = require('../controllers/users_controller')
-router.get('/profile',userController.profile)
-router.get('/privacy',userController.privacy)
+const userController = require('../controllers/users_controller');
+router.get('/profile',userController.profile);
+router.get('/privacy',userController.privacy);
+router.get('/sign-up', userController.signUp);
+router.get('/sign-in', userController.signIn);
 
 
 module.exports = router 
