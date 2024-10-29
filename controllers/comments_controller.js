0c2ea51c69617aa87a641804a70da47d1bd30f57
Mukul Jataria <mukul.jataria@indiamart.com>
2024-10-30 01:34:05 +0530

    Added AJAX to comments MUKUL

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -11,11 +11,30 @@ module.exports.create = async function(req,res){
                     post: req.body.post,
                     user: req.user._id
                 })
-                if(comment){
+                // if(comment){
                     post.comments.push(comment)
                     post.save();  
+
+
+                    if (req.xhr){
+                        // Similar for comments to fetch the user's id!
+                        // comment = await comment.populate('user', 'name').execPopulate();
+                        comment = await comment.populate('user', 'name');
+
+            
+                        return res.status(200).json({
+                            data: {
+                                comment: comment
+                            },
+                            message: "Post created!"
+                        });
+                    }
+        
+        
+                    req.flash('success', 'Comment published!');
+
                     res.redirect('/'); 
-                }
+                // }
                 
             }catch(err){
                 //* handle Error
@@ -38,7 +57,19 @@ module.exports.destroy = async function(req,res){
             await comment.deleteOne(); // Use deleteOne() instead of remove()
             try{
                let post =  await Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}})
-               return res.redirect('back'); 
+                // send the comment id which was deleted back to the views
+                if (req.xhr){
+                    return res.status(200).json({
+                        data: {
+                            comment_id: req.params.id
+                        },
+                        message: "Post deleted"
+                    });
+                }
+
+
+                req.flash('success', 'Comment deleted!');
+                return res.redirect('back'); 
             }catch(err){
                 console.log("Error in deleting comment_id from post")
                 return res.redirect('back'); 
