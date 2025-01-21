3e8517c69b9052aa05b3647648f4a604c09b7f10
Mukul Jataria <mukul.jataria@indiamart.com>
2025-01-21 13:45:29 +0530

    Action and Routes Adde for Likes

--- routes/index.js
+++ routes/index.js
@@ -10,17 +10,11 @@ router.get('/index', homeController.index)
 router.use('/users',require('./users'))
 router.use('/posts',require('./posts'));
 router.use('/comments', require('./comments'));
-// router.use('/likes', require('./likes'))
+router.use('/likes', require('./likes'))
 
 router.use('/api', require('./api'));
 
-
-
 //for any further routes, access from here
 // router.use('/routerName', require(./routerfile));
-
-
- 
-
 
 module.exports = router
