f96707d321a9514548a49c140d0c1bd320ee64d1
Mukul Jataria <mukul.jataria@indiamart.com>
2025-02-02 16:23:11 +0530

    Chat Room Created

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const customMware = require('./config/middleware')
 
 // setup the chat server to be used with socket.io
 const chatServer = require('http').Server(app)
-const chatSocket = require('./config/chat_sockets').chatSockets(chatServer);
+const chatSockets = require('./config/chat_sockets').chatSockets(chatServer);
 chatServer.listen(5000);
 console.log('chat server is listening on port 5000')
 
